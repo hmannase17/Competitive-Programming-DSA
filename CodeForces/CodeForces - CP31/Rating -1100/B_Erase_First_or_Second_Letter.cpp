d2f353051f51d21ea016e8657691f6a1a821cba0
Hansraj-Windows <199348782+hmannase17@users.noreply.github.com>
2026-02-25 13:13:51 +0530

    Daily CP31 1100

--- CodeForces/CodeForces - CP31/Rating -1100/B_Erase_First_or_Second_Letter.cpp
+++ CodeForces/CodeForces - CP31/Rating -1100/B_Erase_First_or_Second_Letter.cpp
@@ -1,81 +1,28 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <cstring>
-#include <cmath>
-#include <cstdlib>
-#include <cstdio>
-#include <climits>
-#include <cfloat>
-#include <cassert>
-#include <vector>
-#include <array>
-#include <deque>
-#include <list>
-#include <stack>
-#include <queue>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <algorithm>
-#include <numeric>
-#include <functional>
-#include <utility>
-#include <bitset>
-#include <chrono>
-#include <random>
-#include <fstream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
 typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
-#define endl '\n'
-
-void fastIO(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-}
-
-ll gcd(ll a, ll b) {
-    return (b == 0) ? a : gcd(b, a % b);
-}
-
-ll lcm(ll a, ll b) {
-    return (a / gcd(a, b)) * b;
-}
-
-unordered_set<string> st;
-
-int recur(string& s , int idx){
-    if(idx >= s.length()) return ;
-
-    int len = s.length() - 1 - idx ;
-    string tmp = s.substr(s.begin() + idx , len);
-    if(!st.count(tmp)) st.insert(tmp);
-
-    recur(s , idx+1);
-    recur(s , idx+2);
-}
-
-void solve(){
-    int t ;
-    while(t--){
-        int n ;
-        cin >> n ;
-        string s ;
-        cin >> s ;
-
-        cout << recur(s , 0) << endl;
-    }
-
-
-}
 
 int main() {
-    fastIO();
-    solve();
-    return 0;
+	int t = 1; 
+	cin >> t;
+
+	while (t--) {
+		int n; 
+		cin >> n;
+		string s; 
+		cin >> s;
+		
+		map<char, int> freq;
+		int ct = 0; 
+
+		vector<int> distinct(n, 0);		
+		for (int i = 0; i<n; i++) { 
+			freq[s[i]]++;
+			if (freq[s[i]] == 1) ct++; 
+			distinct[i] = ct; 
+		}
+		ll ans = 0; 
+		for (int i = 0; i<n; i++) ans += distinct[i];
+		cout << ans << endl; 
+	}
 }
