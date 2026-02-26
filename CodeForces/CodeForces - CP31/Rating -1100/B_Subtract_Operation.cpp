3dc1d028e943014dc12886841767838df22aae28
Hansraj-Windows <199348782+hmannase17@users.noreply.github.com>
2026-02-27 00:10:44 +0530

    Daily CP31 1100

---
+++ CodeForces/CodeForces - CP31/Rating -1100/B_Subtract_Operation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve() {
+	ll n, k;
+	cin >> n >> k; 
+	vector<ll> v(n); 
+	for (int i = 0; i<n; i++) cin >> v[i];
+
+	map<ll, bool> mp; 
+	for (auto it : v) mp[it] = true;
+
+	for (int i = 0; i<n; i++) {
+		if (mp.find(v[i] - k) != mp.end()) {
+			cout << "YES" << endl; 
+			return;
+		}
+	}
+	cout << "NO" << endl; 
+}
+
+int main() {
+	int t;
+	cin >> t; 
+	while (t--) solve();
+}
