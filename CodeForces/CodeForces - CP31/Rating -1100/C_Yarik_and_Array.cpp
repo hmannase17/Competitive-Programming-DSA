cd083c3c5516ec6e7372e733ffb5876c61b12293
Hansraj-Windows <199348782+hmannase17@users.noreply.github.com>
2026-03-10 15:37:17 +0530

    Daily CP31 1100

---
+++ CodeForces/CodeForces - CP31/Rating -1100/C_Yarik_and_Array.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	int t; 
+	cin >> t;
+	while (t--){
+		ll n; 
+		cin >> n;
+		vector<ll> v(n); 
+		for (int i = 0; i<n; i++) cin >> v[i];
+
+		int i = 0, j = 0; 
+		ll ans = INT_MIN, sum = 0; 
+
+		while (j<n) { 
+			if (sum<0){
+				sum = 0;
+				i = j;
+			}
+
+			if (i<j) {				
+				if ((v[j] ^ v[j-1]) & 1) sum += v[j];
+				else {
+					sum = v[j];
+					i = j;
+				}
+			} 
+            else sum = v[j];
+			
+			ans = max(ans, sum);
+			j++;
+		}
+		cout << ans << endl;
+	}
+}
