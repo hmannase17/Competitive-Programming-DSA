10f33ebf94d248d1911322531258083dabbd3ce4
Hansraj-Windows <199348782+hmannase17@users.noreply.github.com>
2026-03-13 16:13:39 +0530

    Daily CP31 1100

---
+++ CodeForces/CodeForces - CP31/Rating -1100/B_Collecting_Game.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	int t; 
+	cin >> t;
+	while (t--) {
+		ll n , a; 
+		cin >> n;
+		vector<pair<ll, ll>> v; 
+        vector<ll> pre(n); 
+		
+		for (int i = 0; i<n; i++){ 
+			cin >> a;
+			v.push_back({a, i});
+		}
+
+		sort(v.begin(), v.end()); 
+		pre[0] = v[0].first;
+		for (int i = 1; i<n; i++) pre[i] = pre[i - 1] + v[i].first;
+
+		vector<ll> ans(n); 
+
+		for (int i = 0; i<n; i++) { 
+			int j = i;
+			int found = i;
+			while (j<n) {
+				pair<ll, ll> temp = {pre[j] + 1, INT_MIN};
+				ll idx = lower_bound(v.begin(), v.end(), temp) - v.begin();
+				idx--;
+				if (idx == j) break;
+				found += idx - j;
+				j = idx;
+			}
+			ans[v[i].second] = found;
+		}
+
+		for (int i = 0; i<n; i++) cout << ans[i] << " ";
+		cout << endl;
+	}
+}
