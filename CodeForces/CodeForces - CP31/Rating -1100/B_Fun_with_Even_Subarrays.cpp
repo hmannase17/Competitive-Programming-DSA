a5194209278cc92bce8911dee245821968a1a49f
Hansraj-Windows <199348782+hmannase17@users.noreply.github.com>
2026-03-14 14:48:14 +0530

    Daily CP31 1100

---
+++ CodeForces/CodeForces - CP31/Rating -1100/B_Fun_with_Even_Subarrays.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t; 
+	while (t--) {
+		int n;
+		cin >> n; 
+		vector<int> v(n); 
+		for (int i = 0; i<n; i++) cin >> v[i]; 
+		
+		int ans = 0, i = n-1; 
+		while (i >= 0 && v[i] == v[n-1]) i--;
+		
+		if (i == -1) {
+			cout << 0 << endl;
+			continue;
+		}
+		
+		while (i >= 0) {
+			i -= (n-1-i); 
+			ans++; 
+			while (i >= 0 && v[i] == v[n-1]) i--;
+		}
+		cout << ans << endl; 
+	}
+}
