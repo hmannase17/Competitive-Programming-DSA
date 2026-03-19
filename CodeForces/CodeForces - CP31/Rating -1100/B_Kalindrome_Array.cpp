d0e7c0c6bc01f6bed468eb1d6b5b7511bc14b759
Hansraj-Windows <199348782+hmannase17@users.noreply.github.com>
2026-03-20 00:02:54 +0530

    Daily CP31 1100

--- CodeForces/CodeForces - CP31/Rating -1100/B_Kalindrome_Array.cpp
+++ CodeForces/CodeForces - CP31/Rating -1100/B_Kalindrome_Array.cpp
@@ -1,55 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
-typedef long long ll;
-#define endl '\n'
+bool check(vector<ll>& a, int x) {
+	int n = a.size();
+	vector<ll> b;
+	
+	for (int i = 0; i<n; i++) {
+		if (a[i] != x) b.push_back(a[i]);
+	}
 
-void fastIO(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    int m = b.size();	
+	for (int i = 0; i<m; i++) {
+		if (b[i] != b[m-i-1]) return false; 
+	}
+	return true; 
 }
 
-bool valid(vector<ll>& a , ll m , ll x){
-    ll curr = 0;
-    for(ll& x : a) curr += max(0LL , m-x);
-    return curr <= x;
+void solve() {
+	ll n;
+	cin >> n;
+	vector<ll> a(n);
+	
+	for (int i = 0; i<n; i++) cin >> a[i];
+	
+	for (int i = 0; i<n; i++) {
+		if (a[i] != a[n-i-1]) {	
+			if (check(a, a[i]) || check(a, a[n-i-1])) cout << "YES" << endl;
+			else cout << "NO" << endl;
+			return;
+		}
+	}
+	cout << "YES" << endl; 
 }
 
-void solve(){
-    int t;
-    cin >> t;
-    while(t--){
-        ll n;
-        cin >> n ;
-        vector<ll> a(n);
-        for(int i = 0; i<n; i++) cin >> a[i];
-
-        ll l = 0, r = n-1;
-        int x = -1;
-
-        while(l <= r){
-            if(a[l] != a[r]){
-                if(x == -1){
-                    x = a[r];
-                }
-                if(a[l] == x) l++ ;
-                else if(a[r] == x) r-- ;
-                else {
-                    cout << "NO\n" ;
-                    return ;
-                }
-            }
-            else {
-                l++ ; 
-                r-- ;
-            }
-        }
-        cout << "YES" << endl;
-    }
-}
-
-int main(){
-    fastIO();
-    solve();
-    return 0;
+int main() {
+	int t;
+	cin >> t;
+	while (t--) solve();
 }
